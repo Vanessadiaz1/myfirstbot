b054673d192970c5ee3a764dc52ab5a526424802
Vanessadiaz1 <vanessad72@hotmail.com>
2017-03-07 11:34:43 -0800

    added chatbot functionality

---
+++ myfirstbot.js
@@ -0,0 +1,18 @@
+var builder     = require('botbuilder');
+var connector   = new builder.ConsoleConnector().listen();
+var bot         = new builder.UniversalBot(connector);  
+
+bot.dialog ('/', [
+
+    session => { builder.Prompts.text (session, "what is your name?") },
+    (session,result) => { 
+        if ( result.response == 'Vanessa')
+        {
+            session.send("cool name!");
+        }
+        else
+        {
+            session.send("that name sux");
+        }
+    }
+])
